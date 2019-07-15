f28ce60725a186bf147ebbfb942354293d376d7c
Yan Martins <yan.martins@gmail.com>
2019-07-15 12:38:52 +0100

    chore: About linter

--- src/components/HathorLogo.js
+++ src/components/HathorLogo.js
@@ -6,7 +6,7 @@ import hathorLogo from '../assets/images/hathor-logo.png';
 const HathorLogo = props => (
   <Image
     source={hathorLogo}
-    style={{ height: 30 }}
+    style={{ height: 30, width: 170 }}
     resizeMode="contain"
     {...props}
   />
