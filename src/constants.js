507e3204e29e8afd423c7829c60c4465d91aa566
Yan Martins <yan.martins@gmail.com>
2019-07-29 16:57:53 +0100

    chore: add license (#2)

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // Workaround to prevent error when using locale in android
 import 'intl';
 import 'intl/locale-data/jsonp/en';
