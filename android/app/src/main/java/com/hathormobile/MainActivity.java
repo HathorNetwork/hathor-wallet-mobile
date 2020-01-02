5624a341761f5ecf2cf45c2cf2e025d2adb8f791
Pedro Ferreira <phsf.pedro@gmail.com>
2020-01-02 03:57:06 -0300

    fix: changing android app name to the same one in the app.json

--- android/app/src/main/java/com/hathormobile/MainActivity.java
+++ android/app/src/main/java/com/hathormobile/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "Hathor Wallet";
+        return "HathorMobile";
     }
 
     @Override
