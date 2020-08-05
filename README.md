a7fd942fa15ec0e82544d7277da5317b289f1e23
Pedro Ferreira <pedro@hathor.network>
2020-08-05 13:02:46 -0300

    feat: add js error handling (#75)
    * feat: adding error handler for js errors
    
    * feat: adding sentry reporting js error
    
    * feat: adding podfile install files and test code
    
    * refactor: removing native error handlers failed attempts
    
    * refactor: removing unused imports
    
    * chore: fixing linter
    
    * chore: updating package lock
    
    * chore: updating podfile lock
    
    * feat: add error handling test as a multiple touch on about logo
    
    * chore: improving comments and error message
    
    * chore: fix linter errors
    
    * feat: adding translation markups and pt translations
    
    * feat: removing sentry properties from git
    
    * chore: add sentry properties do gitignore
    
    * chore: updating locale po files
    
    * chore: adding sentry config details on README
    
    * feat: remove copy data button and add key info on sentry report
    
    * chore: fix linter errors
    
    * feat: adding close button to error handling alert
    
    * chore: adding last locale texts
    
    * refactor: moving sentry dsn to the config file
    
    * feat: after showing error alert, we now show a modal that can't be dismissed, so user needs to restart the app
    
    * chore: adding updated po files with portuguese translations
    
    * chore: removing fuzzy translations comments

--- README.md
+++ README.md
@@ -6,6 +6,36 @@
 
 `npm run node-hack`
 
+### Podfile install for iOS
+
+#### Install cocoapods
+
+Cocoapods is an iOS package manager. Sentry needs to add some native codes, so we must install an iOS native lib of Sentry.
+
+Actually Apple already released a new package manager 'Swift Package Manager' but it's not supported for the React native version we use.
+
+`sudo gem install cocoapods`
+
+`cd ios && pod install`
+
+### Sentry configuration
+
+We use Sentry to allow users to send crash reports.
+
+There is one file that must be created on ios and android folders. This file has a token used to upload some symbols to Sentry.
+https://github.com/getsentry/sentry-react-native/issues/112#issuecomment-309980345
+
+This token is only used when releasing an app version, no need to have it for development.
+
+So you must create `ios/sentry.properties` and `android/sentry.properties` files with the following content in both:
+
+```
+defaults.url=https://sentry.io/
+defaults.org=ORG-NAME
+defaults.project=PROJECT-NAME
+auth.token=YOUR-SENTRY-TOKEN
+```
+
 ## Run
 
 `npm run ios` to open on iOS simulator, or `npm run ios -- --simulator "iPhone 11"`
---
+++ src/errorHandler.js
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { Alert } from 'react-native';
+import hathorLib from '@hathor/wallet-lib';
+import * as Sentry from '@sentry/react-native';
+import VersionNumber from 'react-native-version-number';
+import { t } from 'ttag';
+import { SENTRY_DSN } from './constants';
+import { store } from './reducer';
+import { setErrorModal } from './actions';
+
+/**
+ * Send error to Sentry
+ */
+const sentryReportError = (error) => {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+  });
+  Sentry.withScope((scope) => {
+    scope.setExtra('App version', JSON.stringify(VersionNumber));
+    const data = [];
+    const accessData = hathorLib.wallet.getWalletAccessData();
+    for (const [key, value] of Object.entries(accessData)) {
+      const obj = {
+        key,
+        type: typeof value,
+      };
+      if (typeof value === 'string') {
+        obj.size = value.length;
+      }
+      data.push(obj);
+    }
+    scope.setExtra('Access information', data);
+    Sentry.captureException(error);
+  });
+
+  store.dispatch(setErrorModal(true));
+};
+
+/**
+ * Capture all JS exceptions that might happen and show an alert to the user
+ */
+export const errorHandler = (error, isFatal) => {
+  if (isFatal) {
+    Alert.alert(
+      t`Unexpected error occurred`,
+      t`Unfortunately an unhandled error happened and you will need to restart your app.\n\nWe kindly ask you to report this error to the Hathor team clicking on the button below.\n\nNo sensitive data will be shared.`,
+      [
+        {
+          text: t`Report error`,
+          onPress: () => {
+            sentryReportError(error);
+          }
+        },
+        {
+          text: t`Close`,
+          onPress: () => {
+            store.dispatch(setErrorModal(false));
+          }
+        }
+      ],
+      { cancelable: false }
+    );
+  } else {
+    // So that we can see it in the ADB logs in case of Android if needed
+    console.log('Unhandled not fatal error', error);
+  }
+};
