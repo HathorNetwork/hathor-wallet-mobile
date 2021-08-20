c3947439ba5a4fa3f3e870a876bdc4fb6285bfb3
André Abadesso <andre.abadesso@gmail.com>
2021-08-19 22:40:00 -0300

    chore: dependencies upgraded, podfile building

---
+++ react-native.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./assets/fonts/'],
+  dependencies: {}, // make sure this dependencies are all valid installed packages or empty if you don't need it
+};
