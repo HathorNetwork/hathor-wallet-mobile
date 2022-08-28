dd16ff97dbdb185d5608bdd8c2cb8d38aa8b0c26
André Abadesso <andre.abadesso@gmail.com>
2022-08-28 18:38:04 -0300

    feat: loading token balances when a new token is added

--- src/sagas/tokens.js
+++ src/sagas/tokens.js
@@ -1,12 +1,12 @@
 import {
+  takeEvery,
+  delay,
   all,
   put,
-  takeLatest,
-  delay,
 } from 'redux-saga/effects';
 
 function* fetchTokenBalance(action) {
-  yield delay(2000);
+  yield delay(4000);
 
   yield put({
     type: 'TOKEN_FETCH_BALANCE_SUCCESS',
@@ -15,14 +15,27 @@ function* fetchTokenBalance(action) {
 }
 
 function* loadTokenMetadata() {
-  yield put({
-    type: 'teta',
-  });
+}
+
+function* routeTokenChange(action) {
+  switch (action.type) {
+    default:
+    case 'SET_TOKENS':
+      for (const token of action.payload) {
+        yield put({ type: 'TOKEN_FETCH_BALANCE_REQUESTED', payload: token.uid });
+      }
+      break;
+    case 'NEW_TOKEN':
+      yield put({ type: 'TOKEN_FETCH_BALANCE_REQUESTED', payload: action.payload.uid });
+      break;
+  }
 }
 
 export function* saga() {
   yield all([
-    takeLatest('LOAD_TOKEN_METADATA_REQUESTED', loadTokenMetadata),
-    takeLatest('TOKEN_FETCH_BALANCE_REQUESTED', fetchTokenBalance),
+    takeEvery('LOAD_TOKEN_METADATA_REQUESTED', loadTokenMetadata),
+    takeEvery('TOKEN_FETCH_BALANCE_REQUESTED', fetchTokenBalance),
+    takeEvery('NEW_TOKEN', routeTokenChange),
+    takeEvery('SET_TOKENS', routeTokenChange),
   ]);
 }
