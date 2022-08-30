94991a192e48ad3d7fe01ef806a724f4493979ee
André Abadesso <andre.abadesso@gmail.com>
2022-08-30 14:04:00 -0300

    feat: token history saga

--- src/sagas/helpers.js
+++ src/sagas/helpers.js
@@ -1,4 +1,20 @@
 // Helper method to wait until a specific action with a specific payload, this should be a helper
-export const specificTypeAndPayload = (type, payload) => (action) => (
-  action.payload === payload && action.type === type
-);
+export const specificTypeAndPayload = (type, payload) => (action) => {
+  if (type !== action.type) {
+    return false;
+  }
+
+  const keys = Object.keys(action);
+
+  for (const key of keys) {
+    if (key === 'type') {
+      continue;
+    }
+
+    if (action.payload[key] !== payload[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
--- src/sagas/tokens.js
+++ src/sagas/tokens.js
@@ -1,24 +1,80 @@
 import {
   takeEvery,
+  select,
   delay,
+  call,
   all,
   put,
 } from 'redux-saga/effects';
+import { TxHistory } from '../models';
+
+const mapTokenHistory = (element, token) => {
+  const data = {
+    txId: element.txId,
+    timestamp: element.timestamp,
+    balance: element.balance,
+    // in wallet service this comes as 0/1 and in the full node comes with true/false
+    voided: Boolean(element.voided),
+    tokenUid: token
+  };
+  return new TxHistory(data);
+};
+
 
 function* fetchTokenBalance(action) {
-  yield delay(4000);
+  const tokenId = action.payload;
 
-  yield put({
-    type: 'TOKEN_FETCH_BALANCE_SUCCESS',
-    payload: action.payload,
-  });
+  try {
+    const wallet = yield select((state) => state.wallet);
+    const [token] = yield call(wallet.getBalance.bind(wallet), tokenId);
+    const balance = {
+      available: token.balance.unlocked,
+      locked: token.balance.locked,
+    };
+
+    yield put({
+      type: 'TOKEN_FETCH_BALANCE_SUCCESS',
+      payload: {
+        tokenId,
+        data: balance,
+      },
+    });
+  } catch (e) {
+    yield put({
+      type: 'TOKEN_FETCH_BALANCE_FAILED',
+      payload: { tokenId },
+    });
+  }
 }
 
-function* loadTokenMetadata() {
+function* fetchTokenHistory(action) {
+  const { tokenId } = action;
+
+  yield delay(1500);
+
+  try {
+    const wallet = yield select((state) => state.wallet);
+    const response = yield call(wallet.getTxHistory.bind(wallet), { token_id: tokenId });
+    const data = response.map((txHistory) => mapTokenHistory(txHistory, tokenId));
+
+    yield put({
+      type: 'TOKEN_FETCH_HISTORY_SUCCESS',
+      payload: {
+        tokenId,
+        data,
+      }
+    });
+  } catch (e) {
+    console.log('E: ', e);
+    yield put({
+      type: 'TOKEN_FETCH_HISTORY_FAILED',
+      payload: { tokenId },
+    });
+  }
 }
 
 function* routeTokenChange(action) {
-  switch (action.type) {
+  /* switch (action.type) {
     default:
     case 'SET_TOKENS':
       for (const token of action.payload) {
@@ -28,13 +84,14 @@ function* routeTokenChange(action) {
     case 'NEW_TOKEN':
       yield put({ type: 'TOKEN_FETCH_BALANCE_REQUESTED', payload: action.payload.uid });
       break;
-  }
+  } */
 }
 
 export function* saga() {
   yield all([
-    takeEvery('LOAD_TOKEN_METADATA_REQUESTED', loadTokenMetadata),
+    // takeEvery('LOAD_TOKEN_METADATA_REQUESTED', loadTokenMetadata),
     takeEvery('TOKEN_FETCH_BALANCE_REQUESTED', fetchTokenBalance),
+    takeEvery('TOKEN_FETCH_HISTORY_REQUESTED', fetchTokenHistory),
     takeEvery('NEW_TOKEN', routeTokenChange),
     takeEvery('SET_TOKENS', routeTokenChange),
   ]);
