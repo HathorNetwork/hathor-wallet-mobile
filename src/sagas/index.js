83bb6396291959979e382e07efa0848508ce1a5f
André Abadesso <andre.abadesso@gmail.com>
2022-09-19 10:26:27 -0300

    docs: added hathor header to new files

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { all, fork } from 'redux-saga/effects';
 import { saga as walletSagas } from './wallet';
 import { saga as tokensSagas } from './tokens';
