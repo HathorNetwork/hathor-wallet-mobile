599237de29932fcc0a56b4c5d390baebf9e8b480
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-01-12 04:05:04 +0000

    feat: (wip) set background message listeners

--- index.js
+++ index.js
@@ -8,9 +8,41 @@
 import './i18nInit';
 import { AppRegistry } from 'react-native';
 import { setJSExceptionHandler } from 'react-native-exception-handler';
+import messaging from '@react-native-firebase/messaging';
+import notifee, { EventType } from '@notifee/react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 import { errorHandler } from './src/errorHandler';
+import { messageHandler } from './src/sagas/pushNotification';
+
+/**
+ * This function is called when the app is in background or quit
+ * and a push notification from notifee is received.
+ *
+ * Notifee is used to interprete the data message received from firebase
+ * and show the notification to the user.
+ */
+notifee.onBackgroundEvent(async ({ type, detail }) => {
+  const { notification, pressAction } = detail;
+
+  if (type === EventType.ACTION_PRESS) {
+    console.log('User pressed notification', notification, pressAction);
+  }
+});
+
+/**
+ * This function is called when the app is in background or quit
+ * and a push notification from firebase is received.
+ */
+const setBackgroundMessageListener = () => {
+  const onBackgroundMessage = async (message) => messageHandler(message);
+  try {
+    messaging().setBackgroundMessageHandler(onBackgroundMessage);
+  } catch (error) {
+    console.error('Error setting background message listener.', error);
+  }
+};
+setBackgroundMessageListener();
 
 // - errorHandler is the callback
 // - second parameter is an optional boolean and defines the error handler behavior
