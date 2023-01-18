c64976427e3700b0f0b7de2007a35d0e0b24f6e9
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-01-18 03:42:53 +0000

    feat: reset deviceId and trigger onAppInitialization after reset

--- src/sagas/pushNotification.js
+++ src/sagas/pushNotification.js
@@ -461,8 +461,10 @@ export function* resetPushNotification() {
   yield STORE.removeItem(pushNotificationKey.settings);
   yield STORE.removeItem(pushNotificationKey.hasBeenEnabled);
   yield STORE.removeItem(pushNotificationKey.optInDismissed);
+  yield STORE.removeItem(pushNotificationKey.deviceId);
   // Reset the state
   yield put(pushReset());
+  yield fork(onAppInitialization);
 }
 
 export function* saga() {
