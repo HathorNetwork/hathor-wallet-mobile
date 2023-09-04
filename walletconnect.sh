ff307c6a095130d31d1edf2124635839ab4a178c
André Abadesso <andre.abadesso@gmail.com>
2023-09-04 20:45:29 -0300

    refactor: using patch to add libraries on walletconnect enable script

--- walletconnect.sh
+++ walletconnect.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 git apply enable-walletconnect.patch
-npm install @walletconnect/core@2.7.6 @walletconnect/web3wallet@1.4.0
 
-cd ios && pod install
+npm install && cd ios && pod install
