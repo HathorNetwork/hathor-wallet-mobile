15b5ff5088c4b16c1aa88b835f0a0b3449d3cf39
Pedro Ferreira <pedro@hathor.network>
2023-09-11 15:39:53 -0300

    fix: validating if device is registered in the FCM before resetting the push notification (#324)
    * fix: validating if device is registered in the FCM before resetting the push notification
    
    * refactor: we should try to unregister device from FCM only if it's registered
    
    * fix: fix typo in console message

--- src/sagas/pushNotification.js
+++ src/sagas/pushNotification.js
@@ -120,9 +120,11 @@ function* createChannelIfNotExists() {
 
 /**
  * Register the device to receive remote messages from FCM.
+ * @param {boolean} showErrorModal if should show error modal in case the device is not registered
+ *
  * @returns {boolean} true if the device is registered on the FCM, false otherwise
  */
-function* confirmDeviceRegistrationOnFirebase() {
+function* confirmDeviceRegistrationOnFirebase(showErrorModal = true) {
   try {
     // Make sure deviceId is registered on the FCM
     if (!messaging().isDeviceRegisteredForRemoteMessages) {
@@ -132,7 +134,9 @@ function* confirmDeviceRegistrationOnFirebase() {
     return true;
   } catch (error) {
     console.error('Error confirming the device is registered on firebase.', error);
-    yield put(onExceptionCaptured(error));
+    if (showErrorModal) {
+      yield put(onExceptionCaptured(error));
+    }
     return false;
   }
 }
@@ -608,12 +612,16 @@ const cleanToken = async () => {
  * This function is responsible for reset the push notification.
  */
 export function* resetPushNotification() {
-  try {
-    // Unregister the device from FCM
-    yield call(cleanToken);
-  } catch (error) {
-    console.error('Error clening token from firebase.', error);
-    yield put(onExceptionCaptured(error));
+  // If we don't have the device registered in the FCM, we shouldn't reset push notification
+  const isDeviceRegistered = yield call(confirmDeviceRegistrationOnFirebase, false);
+  if (isDeviceRegistered) {
+    try {
+      // Unregister the device from FCM
+      yield call(cleanToken);
+    } catch (error) {
+      console.error('Error cleaning token from firebase.', error);
+      yield put(onExceptionCaptured(error));
+    }
   }
 
   // Clean the store
