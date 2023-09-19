8120e6001d668f52ad19deb08e10b9053f4a9c0a
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-09-19 17:29:31 +0100

    test: change test to .tsx extension and add saga-redux mocks

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,11 +1,18 @@
 /* eslint-disable */
-
 import 'react-native';
+import {it, jest} from '@jest/globals';
+
 import React from 'react';
 import App from '../src/App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+
+jest.mock('redux-saga', () => () => ({ run: jest.fn() }));
+jest.mock('redux', () => ({
+  createStore: jest.fn(),
+  applyMiddleware: jest.fn(),
+}));
 
 it('renders correctly', () => {
   renderer.create(<App />);
