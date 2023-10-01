ea5ecd864af5bb871a6310d32290385bc8aa1c22
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-10-01 22:39:58 +0100

    refactor: hardcoded explorer link to networkSettings explorerUrl

--- src/components/PublicExplorerListButton.js
+++ src/components/PublicExplorerListButton.js
@@ -4,12 +4,14 @@ import { Image, Linking } from 'react-native';
 import icShareActive from '../assets/icons/icShareActive.png';
 import { ListButton } from './HathorList';
 import { COLORS } from '../styles/themes';
+import { useSelector } from 'react-redux';
 
 export function PublicExplorerListButton(props) {
   const { txId } = props;
   const explorerIcon = <Image source={icShareActive} width={24} height={24} />;
+  const explorerUrl = useSelector((state) => state.networkSettings.explorerUrl);
   // XXX: maybe we should have this on the constants or utils to check the network
-  const explorerLink = `https://explorer.hathor.network/transaction/${txId}`;
+  const explorerLink = `${explorerUrl}transaction/${txId}`;
 
   return (
     <ListButton title={t`Public Explorer`} button={explorerIcon} onPress={() => { Linking.openURL(explorerLink); }} titleStyle={{ color: COLORS.textColorShadow }} isLast />
