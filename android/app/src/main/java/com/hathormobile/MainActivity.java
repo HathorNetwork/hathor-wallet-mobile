3f7cc264a0c523429aa3a70e39ee81c708c5d2a9
tuliomir <tulio.mir@gmail.com>
2023-10-04 19:16:04 -0300

    chore: helper suggested edits

--- android/app/src/main/java/com/hathormobile/MainActivity.java
+++ android/app/src/main/java/com/hathormobile/MainActivity.java
@@ -2,36 +2,31 @@ package network.hathor.wallet;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "HathorMobile";
-    }
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "HathorMobile";
+  }
 
-    public static class MainActivityDelegate extends ReactActivityDelegate {
-      public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
-        super(activity, mainComponentName);
-      }
-      @Override
-      protected ReactRootView createRootView() {
-        ReactRootView reactRootView = new ReactRootView(getContext());
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * (aka React 18) with two boolean flags.
+   */
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
-        return reactRootView;
-      }
-
-      @Override
-      protected boolean isConcurrentRootEnabled() {
-        // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
-        // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
-        return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-      }
-    }
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
 }
--- metro.config.js
+++ metro.config.js
@@ -5,20 +5,21 @@
  * @format
  */
 
-const blacklist = require('metro-config/src/defaults/exclusionList');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const blacklist = require('@react-native/metro-config/src/defaults/exclusionList');
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
   resolver: {
     blacklistRE: blacklist([
       /node_modules\/.*\/node_modules\/react-native\/.*/,
     ]),
   },
 };
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
