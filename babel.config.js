d9e60c64ed6dedc76d5812780c57741bb9a5cece
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-10-06 22:25:04 +0100

    babel: configure @components and @screens module aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['module-resolver',
+      {
+        root: [
+          './src',
+        ],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens'
+        }
+      }
+    ]
+  ],
 };
