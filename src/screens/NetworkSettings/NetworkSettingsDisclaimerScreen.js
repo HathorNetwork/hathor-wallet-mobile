62227e89f8ed2f2f1b1c9f89356815f71d9e6305
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-10-06 22:47:00 +0100

    feat: add NetworkSettingsDisclaimerScreen

---
+++ src/screens/NetworkSettings/NetworkSettingsDisclaimerScreen.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+import { t } from 'ttag';
+import HathorHeader from '../../components/HathorHeader';
+import NewHathorButton from '../../components/NewHathorButton';
+
+const riskDisclaimerTitleText = t`Risk Disclaimer`.toUpperCase();
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'space-between',
+    padding: 16,
+    paddingBottom: 48,
+  },
+  centeredText: {
+    fontSize: 16,
+  },
+  buttonContainer: {
+    alignSelf: 'stretch',
+  },
+});
+
+export const NetworkSettingsDislaimerNav = Symbol('NetworkSettingsDislaimer').toString();
+
+export function NetworkSettingsDislaimerScreen({ navigation }) {
+  return (
+    <View style={style.container}>
+      <HathorHeader
+        title={riskDisclaimerTitleText}
+        onBackPress={() => navigation.goBack()}
+      />
+      <View style={style.content}>
+        <View>
+          <Text style={style.centeredText}>
+            {t`Do not change this unless you know what your are doing, and under no circustance change this settings based on someone else suggestion, as this can potentially make you susceptible to fraudulent schemes.`}
+          </Text>
+        </View>
+        <View style={style.buttonContainer}>
+          <NewHathorButton
+            onPress={() => navigation.navigate('Settings') }
+            title={t`I understand`} />
+        </View>
+      </View>
+    </View>
+  );
+}
--- src/screens/NetworkSettings/NetworkSettingsFlowStack.js
+++ src/screens/NetworkSettings/NetworkSettingsFlowStack.js
@@ -1,4 +1,5 @@
 import { createStackNavigator } from '@react-navigation/stack';
+import { NetworkSettingsDislaimerNav, NetworkSettingsDislaimerScreen } from './NetworkSettingsDisclaimerScreen';
 
 export const NetworkSettingsFlowNav = Symbol('NetworkSettingsFlowStack').toString();
 
@@ -6,10 +7,12 @@ export const NetworkSettingsFlowStack = ({ navigation }) => {
   const FlowStack = createStackNavigator();
   return (
     <FlowStack.Navigator
-      initialRouteName='NetworkSettingsDisclaimer'
+      initialRouteName={NetworkSettingsDislaimerNav}
       screenOptions={{
         headerShown: false,
       }}
-    />
+    >
+      <FlowStack.Screen name={NetworkSettingsDislaimerNav} component={NetworkSettingsDislaimerScreen} />
+    </FlowStack.Navigator>
   );
 };
