3f8f3bd9b408e348963d73b2ebe8e14b1dd8bf9e
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-10-11 01:26:23 +0100

    feat: set correctly the NetworkSettingsFlowStack
    - if network settings is custom, initialize with the
      NetworkPreSettingsScreen, otherwise initialize with the
      NetworkSettingsDisclaimerNav

--- src/screens/NetworkSettings/NetworkSettingsFlowStack.js
+++ src/screens/NetworkSettings/NetworkSettingsFlowStack.js
@@ -5,17 +5,29 @@
  * LICENSE file in the root directory of this source tree.
  */
 import { createStackNavigator } from '@react-navigation/stack';
+import { useSelector } from 'react-redux';
+import { eq } from 'lodash';
+import { PRE_SETTINGS_MAINNET, PRE_SETTINGS_TESTNET } from '../../constants';
 import { CustomNetworkSettingsNav, CustomNetworkSettingsScreen } from './CustomNetworkSettingsScreen';
 import { NetworkPreSettingsNav, NetworkPreSettingsScreen } from './NetworkPreSettingsScreen';
 import { NetworkSettingsDisclaimerNav, NetworkSettingsDisclaimerScreen } from './NetworkSettingsDisclaimerScreen';
 
 export const NetworkSettingsFlowNav = Symbol('NetworkSettingsFlowStack').toString();
 
+const isCustomNetwork = (networkSettings) => {
+  return !(eq(PRE_SETTINGS_MAINNET, networkSettings) || eq(PRE_SETTINGS_TESTNET, networkSettings));
+};
+
 export const NetworkSettingsFlowStack = ({ navigation }) => {
+  const networkSettings = useSelector((state) => state.networkSettings);
+  const initialNav = isCustomNetwork(networkSettings)
+    ? NetworkPreSettingsNav
+    : NetworkSettingsDislaimerNav;
+
   const FlowStack = createStackNavigator();
   return (
     <FlowStack.Navigator
-      initialRouteName={CustomNetworkSettingsNav}
+      initialRouteName={initialNav}
       screenOptions={{
         headerShown: false,
       }}
