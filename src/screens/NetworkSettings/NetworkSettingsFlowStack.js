d8f157db91b2e2a4792300ff9f91e74826aef2c4
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-10-19 16:45:57 +0100

    chore: add copyright

--- src/screens/NetworkSettings/NetworkSettingsFlowStack.js
+++ src/screens/NetworkSettings/NetworkSettingsFlowStack.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import { createStackNavigator } from '@react-navigation/stack';
 import { NetworkPreSettingsNav, NetworkPreSettingsScreen } from './NetworkPreSettingsScreen';
 import { NetworkSettingsDisclaimerNav, NetworkSettingsDisclaimerScreen } from './NetworkSettingsDisclaimerScreen';
