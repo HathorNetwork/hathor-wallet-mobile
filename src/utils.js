21d027b2806dba6622a806efbf7c028b4bfd5607
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-10-25 22:41:28 +0100

    (3) refactor: hardcoded explorer link to networkSettings explorerUrl (#337)
    * refactor: hardcoded explorer link to networkSettings explorerUrl
    
    * feat: add combineURLs function
    
    * refactor: replace interpolation to combineURLs
    
    * lint: resolve lint issues

--- src/utils.js
+++ src/utils.js
@@ -360,3 +360,16 @@ export const getPushNotificationSettings = (pushNotification) => {
     showAmountEnabled
   };
 };
+
+/**
+ * Creates a new URL by combining the specified URLs
+ *
+ * @param {string} baseURL The base URL
+ * @param {string} relativeURL The relative URL
+ * @returns {string} The combined URL
+ */
+export function combineURLs(baseURL, relativeURL) {
+  return relativeURL
+    ? `${baseURL.replace(/\/+$/, '')}/${relativeURL.replace(/^\/+/, '')}`
+    : baseURL;
+}
