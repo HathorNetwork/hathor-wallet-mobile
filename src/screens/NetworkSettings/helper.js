3697f36e2177e329f61a70fb4939233d09023141
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-10-26 00:11:08 +0100

    (13) feat: add feedback modal to presettings screen (#353)
    * feat: add feedback modal to presettings screen
    
    * fix: lint issues and some missing codes

---
+++ src/screens/NetworkSettings/helper.js
@@ -0,0 +1,21 @@
+import { t } from 'ttag';
+import { NETWORKSETTINGS_STATUS } from '../../constants';
+
+export const feedbackLoadingText = t`Updating custom network settings...`;
+export const feedbackFailedText = t`There was an error while customizing network settings. Please try again later.`;
+
+/**
+ * Check if the network settings status is failed.
+ * @param {object} networkSettingsStatus - status from redux store
+ * @returns {boolean} - true if the status is failed, false otherwise
+ */
+// eslint-disable-next-line max-len
+export const hasFailed = (networkSettingsStatus) => networkSettingsStatus === NETWORKSETTINGS_STATUS.FAILED;
+
+/**
+ * Check if the network settings status is loading.
+ * @param {object} networkSettingsStatus - status from redux store
+ * @returns {boolean} - true if the status is loading, false otherwise
+ */
+// eslint-disable-next-line max-len
+export const isLoading = (networkSettingsStatus) => networkSettingsStatus === NETWORKSETTINGS_STATUS.LOADING;
