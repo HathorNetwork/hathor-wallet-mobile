628689a7142b61ab3480554c62f8e163edebf2fb
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-11-09 13:33:01 +0000

    Update src/utils.js
    Co-authored-by: Pedro Ferreira <pedro@hathor.network>

--- src/utils.js
+++ src/utils.js
@@ -130,7 +130,7 @@ export const str2jsx = (text, fnMap) => {
    * Validates an address
    *
    * @param {string} address Address in base58
-   * @param {Object} instance of Network object from the wallet-lib
+   * @param {Object} network Instance of Network object from the wallet-lib
    *
    * @return {Object} boolean indicating if address is valid and possibly an error message
    */
