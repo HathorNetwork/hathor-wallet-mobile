100e7f2e6fb2978151a9d5c29472330457dc4a2f
tuliomir <tulio.mir@gmail.com>
2023-11-14 16:28:16 -0300

    fix: registered token fetch on QR

--- src/screens/SendScanQRCode.js
+++ src/screens/SendScanQRCode.js
@@ -19,6 +19,7 @@ import { COLORS } from '../styles/themes';
 
 const mapStateToProps = (state) => ({
   wallet: state.wallet,
+  tokens: state.tokens,
 });
 
 class SendScanQRCode extends React.Component {
@@ -42,7 +43,7 @@ class SendScanQRCode extends React.Component {
     if (!qrcode.isValid) {
       this.showAlertError(qrcode.error);
     } else if (qrcode.token && qrcode.amount) {
-      if (await this.props.wallet.storage.isTokenRegistered(qrcode.token.uid)) {
+      if (this.props.tokens.find((stateToken) => stateToken.uid === qrcode.token.uid)) {
         const params = {
           address: qrcode.address,
           token: qrcode.token,
