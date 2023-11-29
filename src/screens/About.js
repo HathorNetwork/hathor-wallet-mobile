1b2105483cbb371cbbe2ced6e4fdfc4fc2fcf12a
tuliomir <tulio.mir@gmail.com>
2023-11-29 18:25:15 -0300

    refactor: reusing render method

--- src/screens/About.js
+++ src/screens/About.js
@@ -54,7 +54,8 @@ export class About extends React.Component {
       // iOS
       const build = VersionNumber.buildVersion.split('.');
       if (build.length !== 3) {
-        return (<Text style={this.style.text}>{JSON.stringify(VersionNumber)} (parse error)</Text>)
+        // Incorrect build version format. Inform the user but do not crash.
+        return this.renderVersionText(VersionNumber.appVersion, `${VersionNumber.buildVersion} (parse error)`);
       }
       if (build[0] === '0') {
         // This is a release candidate build
