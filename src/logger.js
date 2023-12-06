0a1885309ae97377a3c26662e0917c45b5e5db18
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-12-06 17:23:36 +0000

    feat: add copyright to logger.js and add docstring

--- src/logger.js
+++ src/logger.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * Wraps the built-in console to add scope to messages.
+* @param {string} scope - prefix each message.
+*
+* @example
+* const log = logger('my-scope');
+* log.debug('a custom message');
+* // output: [my-scope] a custom message
+ */
 export const logger = (scope) => ({
   log(msg) {
     console.log(`[${scope}] ${msg}`);
