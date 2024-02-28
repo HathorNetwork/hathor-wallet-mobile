5eb87194fef5709247830b04a13cf503aa94a50d
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2024-02-28 23:55:45 +0000

    refactor: HathorHeader extracting inner components to standalone components

--- src/components/HathorHeader.js
+++ src/components/HathorHeader.js
@@ -16,61 +16,71 @@ import chevronLeft from '../assets/icons/chevron-left.png';
 import closeIcon from '../assets/icons/icCloseActive.png';
 import { COLORS, STYLE } from '../styles/themes';
 
-const HathorHeader = (props) => {
-  const renderBackButton = () => {
-    if (props.onBackPress) {
-      return (
-        <View style={[styles.iconWrapper, { justifyContent: 'flex-start' }]}>
-          <TouchableOpacity onPress={props.onBackPress}>
-            <Image source={chevronLeft} width={24} height={24} />
-          </TouchableOpacity>
-        </View>
-      );
-    }
-    return <View style={styles.iconWrapper} />;
-  };
-
-  const CancelButton = () => (
-    <SimpleButton
-      icon={closeIcon}
-      onPress={props.onCancel}
-    />
-  );
-
-  const renderHeaderRight = () => {
-    const element = (props.onCancel ? <CancelButton /> : props.rightElement);
-    return (
-      <View style={[styles.iconWrapper, { justifyContent: 'flex-end' }]}>
-        {element}
-      </View>
-    );
-  };
-
-  const renderHeaderCentral = () => {
-    if (props.withLogo) {
-      return (
-        <Logo
-          style={{ height: 22, width: 100 }}
-        />
-      );
-    }
-    return <Text>{props.title}</Text>;
-  };
-
-  let extraStyle = {};
-  if (props.withBorder) {
-    extraStyle = { borderBottomWidth: 1 };
-  }
-
+const HathorHeader = ({
+  title,
+  rightElement,
+  withLogo,
+  withBorder,
+  wrapperStyle,
+  onBackPress,
+  onCancel,
+}) => {
   return (
-    <View style={[styles.wrapper, props.wrapperStyle, extraStyle]}>
+    <View style={[styles.wrapper, wrapperStyle, applyExtraStyle(withBorder)]}>
       <View style={styles.innerWrapper}>
-        {renderBackButton()}
-        {renderHeaderCentral()}
-        {renderHeaderRight()}
+        <LeftComponent onBackPress={onBackPress} />
+        <CentralComponent title={title} withLogo={withLogo} />
+        <RightComponent rightElement={rightElement} onCancel={onCancel} />
       </View>
     </View>
   );
+};
+
+const CancelButton = () => (
+  <SimpleButton
+    icon={closeIcon}
+    onPress={onCancel}
+  />
+);
+
+const LeftComponent = ({ onBackPress }) => {
+  if (onBackPress) {
+    return (
+      <View style={[styles.iconWrapper, { justifyContent: 'flex-start' }]}>
+        <TouchableOpacity onPress={onBackPress}>
+          <Image source={chevronLeft} width={24} height={24} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  return <View style={styles.iconWrapper} />;
+};
+
+const CentralComponent = ({ title, withLogo }) => {
+  if (withLogo) {
+    return (
+      <Logo
+        style={{ height: 22, width: 100 }}
+      />
+    );
+  }
+  return <Text>{title}</Text>;
+};
+
+const RightComponent = ({ rightElement, onCancel }) => {
+  const element = (onCancel ? <CancelButton /> : rightElement);
+  return (
+    <View style={[styles.iconWrapper, { justifyContent: 'flex-end' }]}>
+      {element}
+    </View>
+  );
+};
+
+const applyExtraStyle = (withBorder) => {
+  if (withBorder) {
+    return { borderBottomWidth: 1 };
+  }
+  return {};
 };
 
 const styles = StyleSheet.create({
