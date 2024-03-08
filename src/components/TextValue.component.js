4d6acc92f12c185c8dc1e33749f23ab78eaeb42a
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2024-03-08 01:48:16 +0000

    feat: improve TextValue component

--- src/components/TextValue.component.js
+++ src/components/TextValue.component.js
@@ -4,8 +4,12 @@ import {
   Text,
 } from 'react-native';
 
-export const TextValue = ({ children }) => (
-  <Text style={[styles.textValue]}>{children}</Text>
+export const TextValue = ({ bold, pb4, children }) => (
+  <Text style={[
+    styles.textValue,
+    bold && styles.bold,
+    pb4 && styles.pb4,
+  ]}>{children}</Text>
 );
 
 const styles = StyleSheet.create({
@@ -13,5 +17,11 @@ const styles = StyleSheet.create({
     fontSize: 14,
     lineHeight: 20,
     color: 'black',
+  },
+  pb4: {
+    paddingBottom: 4,
+  },
+  bold: {
+    fontWeight: 'bold',
   },
 });
