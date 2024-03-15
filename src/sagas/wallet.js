fedc5085c9475a8c3d3f41c60c1370ba21776367
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2024-03-15 16:28:19 +0000

    fix: missing transaction after reload (#442)
    * feat: add force param to featchTokenData
    
    * chore: improve documentation

--- src/sagas/wallet.js
+++ src/sagas/wallet.js
@@ -287,7 +287,7 @@ export function* startWallet(action) {
 
 /**
  * This saga will load both HTR and DEFAULT_TOKEN (if they are different)
- * and dispatch actions to asynchronously load all registered tokens.
+ * and dispatch actions to asynchronously load all registered tokens forcefully.
  *
  * Will throw an error if the download fails for any token.
  * @returns {string[]} Array of token uid
@@ -298,9 +298,12 @@ export function* loadTokens() {
 
   // fetchTokenData will throw an error if the download failed, we should just
   // let it crash as throwing an error is the default behavior for loadTokens
-  yield call(fetchTokenData, htrUid);
+  yield call(fetchTokenData, htrUid, true);
 
   if (customTokenUid !== htrUid) {
+    // custom tokens doesn't need to be forced to download because its history status
+    // will be marked as invalidated, and history will get requested the next time a user
+    // enters the history screen.
     yield call(fetchTokenData, customTokenUid);
   }
 
@@ -628,6 +631,7 @@ export function* onWalletReloadData() {
   }
 
   try {
+    // Here we force the download of tokens history
     const registeredTokens = yield call(loadTokens);
 
     const customTokenUid = DEFAULT_TOKEN.uid;
