872185e8013ef7a990fa3a02b64213ea2c610ef8
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2024-04-09 16:01:47 +0100

    feat: improve aprearance of push notification opt-in modal

--- src/components/AskForPushNotification.js
+++ src/components/AskForPushNotification.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { StyleSheet, Text } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { t } from 'ttag';
 import { pushDismissOptInQuestion } from '../actions';
-import ActionModal from './ActionModal';
+import { ModalBase } from './ModalBase.component';
 
 export default function AskForPushNotification(props) {
   const showOptIn = useSelector((state) => state.pushNotification.showOptInQuestion);
@@ -17,19 +18,34 @@ export default function AskForPushNotification(props) {
     dispatch(pushDismissOptInQuestion());
   };
 
-  const pushNotificationOptInModal = () => (
-    <ActionModal
-      title={t`Do you want to enable push notifications for this wallet?`}
-      text={t`You can always change this later in the settings menu`}
-      button={t`Yes, enable`}
-      onAction={onEnablePushNotifications}
-      onDismiss={putDismissOptInQuestion}
-    />
+  return (
+    <ModalBase show={showOptIn} onDismiss={putDismissOptInQuestion}>
+      <ModalBase.Title>{t`Do you want to enable push notifications for this wallet?`}</ModalBase.Title>
+      <ModalBase.Body style={styles.body}>
+        <Text style={[styles.text]}>{t`You can always change this later in the settings menu.`}</Text>
+      </ModalBase.Body>
+      <ModalBase.Button
+        title={t`Yes, enable`}
+        onPress={onEnablePushNotifications}
+      />
+      <ModalBase.DiscreteButton
+        title={t`No, not now`}
+        onPress={putDismissOptInQuestion}
+      />
+    </ModalBase>
   );
-
-  if (showOptIn) {
-    return pushNotificationOptInModal();
-  }
-
-  return null;
 }
+
+const styles = StyleSheet.create({
+  body: {
+    paddingBottom: 20,
+  },
+  fieldContainer: {
+    width: '100%',
+    paddingBottom: 4,
+  },
+  text: {
+    fontSize: 14,
+    lineHeight: 20,
+  },
+});
