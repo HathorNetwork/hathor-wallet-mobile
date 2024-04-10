601004b036feae5c78f91636754480d288bfdb9c
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2024-04-10 17:10:06 +0100

    feat: add icons [4] (#453)
    * feat: add ActionDot icon
    * feat: add CircleCheck.icon
    * feat: add CircleClock icon
    * feat: add CircleError icon

---
+++ src/components/Icons/helper.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} size
+ * @example
+ * getViewBox(24)
+ * // return `0 0 24 24`
+ */
+export const getViewBox = (size) => `0 0 ${size} ${size}`;
+
+/**
+ * @param {number} size
+ * @param {number} baseSize
+ * @example
+ * getScale(48, 24)
+ * // return `scale(2)`
+ */
+export const getScale = (size, baseSize) => `scale(${size / baseSize})`;
