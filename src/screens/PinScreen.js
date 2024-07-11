aac23ecc32790a69beb65caa7fc49e69d377ce36
André Abadesso <andre.abadesso@gmail.com>
2024-07-11 10:30:58 -0300

    Revert "refactor: convert PinScreen to a function component"
    This reverts commit 69473153c57efed4451c43d4e517508d43203841.

--- src/screens/PinScreen.js
+++ src/screens/PinScreen.js
@@ -5,10 +5,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import { t } from 'ttag';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigation, useRoute } from '@react-navigation/native';
+
 import { BackHandler, Keyboard, Text, View } from 'react-native';
 import * as Keychain from 'react-native-keychain';
 import { walletUtils, cryptoUtils } from '@hathor/wallet-lib';
@@ -17,105 +17,105 @@ import PinInput from '../components/PinInput';
 import Logo from '../components/Logo';
 import { isBiometryEnabled, getSupportedBiometry } from '../utils';
 import {
-  unlockScreen as unlockScreenAction,
-  startWalletRequested as startWalletRequestedAction,
-  resetOnLockScreen as resetOnLockScreenAction,
-  onExceptionCaptured,
+  lockScreen,
+  unlockScreen,
+  setLoadHistoryStatus,
+  setTempPin,
+  onStartWalletLock,
+  startWalletRequested,
+  resetOnLockScreen,
+  pinScreenResult,
 } from '../actions';
 import { PIN_SIZE } from '../constants';
 import { COLORS } from '../styles/themes';
 import { STORE } from '../store';
 import baseStyle from '../styles/init';
 
-export default function PinScreen({ isLockScreen }) {
-  const wallet = useSelector((state) => state.wallet);
+/**
+ * loadHistoryActive {bool} whether we still need to load history
+ */
+const mapStateToProps = (state) => ({
+  loadHistoryActive: state.loadHistoryStatus.active,
+  wallet: state.wallet,
+});
 
-  const route = useRoute();
-  const dispatch = useDispatch();
-  const navigation = useNavigation();
+const mapDispatchToProps = (dispatch) => ({
+  unlockScreen: () => dispatch(unlockScreen()),
+  lockScreen: () => dispatch(lockScreen()),
+  resetOnLockScreen: () => dispatch(resetOnLockScreen()),
+  setLoadHistoryStatus: (active, error) => dispatch(setLoadHistoryStatus(active, error)),
+  setTempPin: (pin) => dispatch(setTempPin(pin)),
+  onStartWalletLock: () => dispatch(onStartWalletLock()),
+  startWalletRequested: (payload) => dispatch(startWalletRequested(payload)),
+  pinScreenResult: (payload) => dispatch(pinScreenResult(payload)),
+});
 
-  const unlockScreen = () => dispatch(unlockScreenAction());
-  const resetOnLockScreen = () => dispatch(resetOnLockScreenAction());
-  const startWalletRequested = (payload) => dispatch(startWalletRequestedAction(payload));
+class PinScreen extends React.Component {
+  static defaultProps = { isLockScreen: false };
 
-  const [pin, setPin] = useState('');
-  const [pinColor, setPinColor] = useState(COLORS.textColor);
-  const [error, setError] = useState(null);
-  const [removing, setRemoving] = useState(false);
+  constructor(props) {
+    super(props);
+    /**
+     * pin {string} Pin entered by the user
+     * error {string} Error message (null if there's no error)
+     */
+    this.state = {
+      pin: '',
+      pinColor: COLORS.textColor,
+      error: null,
+    };
 
-  let focusEvent = null;
-  let canCancel = false;
-  let screenText = t`Enter your PIN Code `;
-  let biometryText = t`Unlock Hathor Wallet`;
+    this.canCancel = false;
+    this.screenText = t`Enter your PIN Code `;
+    this.biometryText = t`Unlock Hathor Wallet`;
+    if (!this.props.isLockScreen) {
+      this.canCancel = props.route.params.canCancel ?? this.canCancel;
+      this.screenText = props.route.params.screenText ?? this.screenText;
+      this.biometryText = props.route.params.biometryText ?? this.biometryText;
+    }
 
-  if (!isLockScreen) {
-    canCancel = route.params.canCancel ?? canCancel;
-    screenText = route.params.screenText ?? screenText;
-    biometryText = route.params.biometryText ?? biometryText;
+    this.focusEvent = null;
   }
 
-  useEffect(() => {
+  componentDidMount() {
     // If the keyboard is being shown, hide it. This screen's keyboard is built differently
     Keyboard.dismiss();
 
     const supportedBiometry = getSupportedBiometry();
     const biometryEnabled = isBiometryEnabled();
     if (supportedBiometry && biometryEnabled) {
-      askBiometricId();
+      this.askBiometricId();
     }
 
-    if (!canCancel) {
+    if (!this.canCancel) {
       // If can't cancel this screen, we must remove the hardware back from android
-      BackHandler.addEventListener('hardwareBackPress', handleBackButton);
+      BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
     }
 
-    focusEvent = navigation.addListener('focus', () => {
+    this.focusEvent = this.props.navigation.addListener('focus', () => {
       // Ensure the keyboard is hidden when the screen is focused, even if it was already mounted
       Keyboard.dismiss();
       // Clear the pin
-      setPin('');
-      setPinColor(COLORS.textColor);
-      setError(null);
+      this.setState({ pin: '', pinColor: COLORS.textColor, error: null });
     });
+  }
 
-    return () => {
-      if (!canCancel) {
-        // Removing event listener
-        BackHandler.removeEventListener(
-          'hardwareBackPress',
-          handleBackButton,
-        );
-      }
-
-      // Removing focus event
-      focusEvent();
-    };
-  }, []);
-
-  useEffect(() => {
-    let timer;
-    if (removing && pin.length > 0) {
-      timer = setTimeout(() => {
-        setPin((prevPin) => prevPin.slice(0, -1));
-      }, 25);
+  componentWillUnmount() {
+    if (!this.canCancel) {
+      // Removing event listener
+      BackHandler.removeEventListener(
+        'hardwareBackPress',
+        this.handleBackButton,
+      );
     }
 
-    if (pin.length === 0 && removing) {
-      setRemoving(false);
-      setError(t`Incorrect PIN Code. Try again.`);
-    }
+    // Removing focus event
+    this.focusEvent();
+  }
 
-    return () => clearTimeout(timer); // Cleanup timeout
-  }, [pin, removing]);
+  handleBackButton = () => true;
 
-  const startRemovingChars = () => {
-    setRemoving(true);
-    setPinColor(COLORS.errorBgColor);
-  };
-
-  const handleBackButton = () => true;
-
-  const askBiometricId = async () => {
+  askBiometricId = async () => {
     try {
       /* getGenericPassword will return either `false` or UserCredentials:
        *
@@ -125,18 +125,59 @@ export default function PinScreen({ isLockScreen }) {
        * }
        */
       const credentials = await Keychain.getGenericPassword({
-        authenticationPrompt: { title: biometryText },
+        authenticationPrompt: { title: this.biometryText },
       });
 
       if (credentials !== false) {
-        dismiss(credentials.password);
+        this.dismiss(credentials.password);
       }
     } catch (e) {
       // No need to do anything here as the user can type his PIN
     }
   };
 
-  const validatePin = async (inputPin) => {
+  dismiss = async (pin) => {
+    if (this.props.isLockScreen) {
+      // in case it's the lock screen, we just have to execute the data migration
+      // method an change redux state. No need to execute callback or go back on navigation
+      await STORE.handleDataMigration(pin);
+      if (!this.props.wallet) {
+        // We have already made sure we have an available accessData
+        // The handleDataMigration method ensures we have already migrated if necessary
+        // This means the wallet is loaded and the access data is ready to be used.
+
+        const words = await STORE.getWalletWords(pin);
+        this.props.startWalletRequested({ words, pin });
+      }
+      this.props.unlockScreen();
+    } else {
+      // dismiss the pin screen first because doing it after the callback can
+      // end up dismissing the wrong screen
+      this.props.navigation.goBack();
+      // execute the callback passing the pin, if any cb was given
+      const cb = this.props.route.params.cb ?? null;
+      if (cb) {
+        cb(pin);
+      }
+
+      if (this.props.route.params.dispatchResponse) {
+        pinScreenResult(pin);
+      }
+    }
+  };
+
+  onChangeText = (text) => {
+    if (text.length > PIN_SIZE) {
+      return;
+    }
+
+    if (text.length === PIN_SIZE) {
+      setTimeout(() => this.validatePin(text), 300);
+    }
+    this.setState({ pin: text, pinColor: COLORS.textColor, error: null });
+  };
+
+  validatePin = async (pin) => {
     try {
       // Validate if we are able to decrypt the seed using this PIN
       // this will throw if the words are not able to be decoded with this
@@ -150,12 +191,12 @@ export default function PinScreen({ isLockScreen }) {
         // The wallet does not have an access data, we can't unlock it
         // This should not happen since we check if the wallet is initialized
         // before showing the unlock screen, but we will handle it anyway
-        dispatch(onExceptionCaptured(
+        this.props.onExceptionCaptured(
           new Error(
             "Attempted to unlock wallet but it wasn't initialized.",
           ),
           true, // Fatal since we can't start the wallet
-        ));
+        );
         return;
       }
 
@@ -171,143 +212,126 @@ export default function PinScreen({ isLockScreen }) {
           pbkdf2Hasher: accessData.pbkdf2Hasher,
         };
       }
-      const pinCorrect = cryptoUtils.checkPassword(wordsEncryptedData, inputPin);
+      const pinCorrect = cryptoUtils.checkPassword(wordsEncryptedData, pin);
 
       if (!pinCorrect) {
-        startRemovingChars();
+        this.removeOneChar();
         return;
       }
 
-      const words = cryptoUtils.decryptData(wordsEncryptedData, inputPin);
+      const words = cryptoUtils.decryptData(wordsEncryptedData, pin);
       // Will throw InvalidWords if the seed is invalid
       walletUtils.wordsValid(words);
     } catch (e) {
-      dispatch(onExceptionCaptured(
-        new Error('User inserted a valid PIN but the app wasn\'t able to decrypt the words'),
+      this.props.onExceptionCaptured(
+        new Error(
+          "User inserted a valid PIN but the app wasn't able to decrypt the words",
+        ),
         true, // Fatal since we can't start the wallet
-      ));
+      );
 
       return;
     }
 
     // Inserted PIN was able to decrypt the words successfully
-    dismiss(inputPin);
+    this.dismiss(pin);
   };
 
-  const onChangeText = (text) => {
-    if (text.length > PIN_SIZE) {
-      return;
-    }
-
-    if (text.length === PIN_SIZE) {
-      setTimeout(() => validatePin(text), 300);
-    }
-
-    setPin(text);
-    setPinColor(COLORS.textColor);
-    setError(null);
+  goToReset = () => {
+    this.props.resetOnLockScreen();
   };
 
-  const dismiss = async (inputPin) => {
-    if (isLockScreen) {
-      // in case it's the lock screen, we just have to execute the data migration
-      // method an change redux state. No need to execute callback or go back on navigation
-      await STORE.handleDataMigration(inputPin);
-      if (!wallet) {
-        // We have already made sure we have an available accessData
-        // The handleDataMigration method ensures we have already migrated if necessary
-        // This means the wallet is loaded and the access data is ready to be used.
+  /*
+   * This function is used when coming back to lock screen from reset screen
+   */
+  backFromReset = () => {
+    // set to same status as before
+    this.props.setLoadHistoryStatus(this.props.loadHistoryActive, false);
+    // show lock screen again
+    this.props.lockScreen();
+    // navigate to dashboard (will be hidden under lock screen)
+    this.props.navigation.navigate('Dashboard');
+  };
 
-        const words = await STORE.getWalletWords(inputPin);
-        startWalletRequested({ words, pin: inputPin });
-      }
-
-      unlockScreen();
+  removeOneChar = () => {
+    const pin = this.state.pin.slice(0, -1);
+    if (pin.length === 0) {
+      this.setState({ pin: '', error: t`Incorrect PIN Code. Try again.` });
     } else {
-      // dismiss the pin screen first because doing it after the callback can
-      // end up dismissing the wrong screen
-      navigation.goBack();
-
-      // execute the callback passing the pin, if any cb was given
-      const cb = route.params.cb ?? null;
-      if (cb) {
-        cb(pin);
-      }
-
-      if (route.params.dispatchResponse) {
-        dispatch(pinScreenResult(pin));
-      }
+      this.setState({ pin, pinColor: COLORS.errorBgColor });
+      setTimeout(() => this.removeOneChar(), 25);
     }
   };
 
-  const goToReset = () => resetOnLockScreen();
+  render() {
+    const renderButton = () => {
+      let title;
+      let onPress;
+      if (this.canCancel) {
+        title = t`Cancel`;
+        onPress = () => this.props.navigation.goBack();
+      } else {
+        title = t`Reset wallet`;
+        onPress = () => this.goToReset();
+      }
 
-  const renderButton = () => {
-    let title;
-    let onPress;
+      return (
+        <SimpleButton
+          onPress={onPress}
+          title={title}
+          textStyle={{
+            textTransform: 'uppercase',
+            color: COLORS.textColorShadow,
+            letterSpacing: 1,
+            padding: 4,
+          }}
+          containerStyle={{ marginTop: 16, marginBottom: 8 }}
+        />
+      );
+    };
 
-    if (canCancel) {
-      title = t`Cancel`;
-      onPress = () => navigation.goBack();
-    } else {
-      title = t`Reset wallet`;
-      onPress = () => goToReset();
-    }
-
-    return (
-      <SimpleButton
-        onPress={onPress}
-        title={title}
-        textStyle={{
-          textTransform: 'uppercase',
-          color: COLORS.textColorShadow,
-          letterSpacing: 1,
-          padding: 4,
-        }}
-        containerStyle={{ marginTop: 16, marginBottom: 8 }}
-      />
-    );
-  };
-
-  const renderPinDigits = () => (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-      }}
-    >
+    const renderPinDigits = () => (
       <View
         style={{
-          marginVertical: 16,
+          flex: 1,
           alignItems: 'center',
-          height: 21,
-          width: 120,
         }}
       >
-        <Logo style={{ height: 21, width: 120 }} />
+        <View
+          style={{
+            marginVertical: 16,
+            alignItems: 'center',
+            height: 21,
+            width: 120,
+          }}
+        >
+          <Logo style={{ height: 21, width: 120 }} />
+        </View>
+        <Text style={{ marginTop: 32, marginBottom: 16 }}>{this.screenText}</Text>
+        <PinInput
+          maxLength={PIN_SIZE}
+          color={this.state.pinColor}
+          value={this.state.pin}
+          onChangeText={this.onChangeText}
+          error={this.state.error}
+        />
+        {renderButton()}
       </View>
-      <Text style={{ marginTop: 32, marginBottom: 16 }}>{screenText}</Text>
-      <PinInput
-        maxLength={PIN_SIZE}
-        color={pinColor}
-        value={pin}
-        onChangeText={onChangeText}
-        error={error}
-      />
-      {renderButton()}
-    </View>
-  );
+    );
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        paddingHorizontal: 16, // Padding ensures a homogeneous background color
-        backgroundColor: baseStyle.container.backgroundColor,
-      }}
-    >
-      {renderPinDigits()}
-    </View>
-  );
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          paddingHorizontal: 16, // Padding ensures a homogeneous background color
+          backgroundColor: baseStyle.container.backgroundColor,
+        }}
+      >
+        {renderPinDigits()}
+      </View>
+    );
+  }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(PinScreen);
