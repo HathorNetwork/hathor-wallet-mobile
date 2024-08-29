87fe9658e605ba8c2ffae497b35c63a0c31319e3
André Abadesso <andre.abadesso@gmail.com>
2024-08-29 11:07:09 -0300

    refactor: removed package.json changes in walletconnect diff

--- walletconnect.sh
+++ walletconnect.sh
@@ -2,4 +2,17 @@
 
 git apply enable-walletconnect.patch
 
+npm install \
+  @walletconnect/core@2.15.1\
+  @walletconnect/web3wallet@1.14.1 \
+  @ethersproject/shims@5.7.0 \
+  @json-rpc-tools/utils@1.7.6 \
+  @react-native-community/netinfo@11.3.1 \
+  @walletconnect/react-native-compat@2.12.2 \
+  ethers@6.13.2 \
+  events@3.3.0 \
+  fast-text-encoding@1.0.6 \
+  react-native-get-random-values@1.11.0 \
+  text-encoding@0.7.0
+
 npm install && cd ios && pod install
