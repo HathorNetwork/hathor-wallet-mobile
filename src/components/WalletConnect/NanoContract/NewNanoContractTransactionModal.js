909233c60a2b9250fb767211cb1c6be6d74381e7
André Abadesso <andre.abadesso@gmail.com>
2024-08-29 11:28:21 -0300

    refactor: removed unused styles from NewNanoContractTransctionModal

--- src/components/WalletConnect/NanoContract/NewNanoContractTransactionModal.js
+++ src/components/WalletConnect/NanoContract/NewNanoContractTransactionModal.js
@@ -13,7 +13,6 @@ import {
 import { t } from 'ttag';
 import { useNavigation } from '@react-navigation/native';
 import { useDispatch, useSelector } from 'react-redux';
-import { COLORS } from '../../../styles/themes';
 import { ModalBase } from '../../ModalBase';
 import { walletConnectReject } from '../../../actions';
 import { WarnDisclaimer } from '../WarnDisclaimer';
@@ -79,45 +78,5 @@ const styles = StyleSheet.create({
   },
   bold: {
     fontWeight: 'bold',
-  },
-  warnContainer: {
-    flexShrink: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderRadius: 8,
-    paddingTop: 12,
-    /* It should have been 12 but it is adjusted to compensate the negative
-     * margin on learnMoreWrapper and the difference between the font size
-     * and the line height, which amounts to 8 points of compensation.
-     */
-    paddingBottom: 20,
-    paddingHorizontal: 16,
-    backgroundColor: COLORS.cardWarning100,
-  },
-  warnContent: {
-    paddingLeft: 8,
-  },
-  warnMessage: {
-    fontSize: 12,
-    lineHeight: 16,
-  },
-  learnMoreWrapper: {
-    display: 'inline-block',
-    /* We are using negative margin here to correct the text position
-     * and create an optic effect of alignment. */
-    marginBottom: -4,
-    paddingLeft: 2,
-    marginRight: 'auto',
-  },
-  learnMoreContainer: {
-    justifyContent: 'flex-start',
-    borderBottomWidth: 1,
-  },
-  learnMoreText: {
-    fontSize: 12,
-    lineHeight: 16,
-    fontWeight: 'bold',
-    color: 'hsla(0, 0%, 25%, 1)',
-  },
+  }
 });
