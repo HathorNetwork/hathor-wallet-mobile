b8449dd9d14344f0097f4c80853424db5f548744
Andre Carneiro <andreluizmrcarneiro@gmail.com>
2025-10-27 12:50:58 -0300

    chore: fix import

--- src/sagas/swap.js
+++ src/sagas/swap.js
@@ -16,7 +16,7 @@ import {
 import { SagaCancellationException } from 'redux-saga';
 import { get } from 'lodash';
 import { getNetworkSettings } from './helpers';
-import { findBestTokenSwap } from '../utils/dozer';
+import { findBestTokenSwap } from '../utils/tokenSwap';
 import {
   types,
   tokenSwapFetchQuoteSuccess,
