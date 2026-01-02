567c9e1385d26bfdecf9eeaeda9ea21690a360d4
Pedro Ferreira <pedro@hathor.network>
2026-01-02 17:51:01 -0300

    refactor: use HTR from constants

--- src/components/Reown/GetUtxosRequest.js
+++ src/components/Reown/GetUtxosRequest.js
@@ -14,6 +14,7 @@ import {
 } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
+import { constants } from '@hathor/wallet-lib';
 import { t } from 'ttag';
 import {
   reownAccept,
@@ -57,7 +58,7 @@ export const GetUtxosRequestData = ({ data }) => {
   const getDisplaySymbol = () => {
     if (isRegistered) return tokenInfo.symbol;
     if (tokenId) return t`Unregistered token`;
-    return 'HTR';
+    return constants.DEFAULT_NATIVE_TOKEN_CONFIG.symbol;
   };
   const displaySymbol = getDisplaySymbol();
 
